fa564760d3e4fdb6787845dc1e33f15d6c4cc20f
Samuel Prayoga Tampubolon <samueltampubolon2002@gmail.com>
2022-05-19 09:39:14 +0700

    Bottom Navigation

--- app/src/main/java/com/example/proyekfinal/MainActivity.kt
+++ app/src/main/java/com/example/proyekfinal/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.proyekfinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.proyekfinal.fragment.AkunFragment
+import com.example.proyekfinal.fragment.HomeFragment
+import com.example.proyekfinal.fragment.KeranjangFragment
 
 class MainActivity : AppCompatActivity() {
+    val fragmentHome: Fragment = HomeFragment()
+    val fragmentAkun: Fragment = AkunFragment()
+    val fragmentKeranjang: Fragment = KeranjangFragment()
+    val fm: FragmentManager = supportFragmentManager
+    var active: Fragment = fragmentHome
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fm.beginTransaction().add(R.id.container, fragmentHome).show(fragmentHome).commit()
+        fm.beginTransaction().add(R.id.container, fragmentAkun).hide(fragmentAkun).commit()
+        fm.beginTransaction().add(R.id.container, fragmentKeranjang).hide(fragmentKeranjang).commit()
     }
 }
